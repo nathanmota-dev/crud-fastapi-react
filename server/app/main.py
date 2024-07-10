8593d5d3cce3c7825b564def408428ab017fde8c
alvarojpr <alvarojosedm9@gmail.com>
2024-07-10 20:12:16 -0300

    feat: /upload and /download to accept List.

--- server/app/main.py
+++ server/app/main.py
@@ -1,3 +1,16 @@
+# NA PASTAR SERVER, E NÃO NA APP.
+# uvicorn app.main:app --reload --root-path server
+
+# Vá para a aba "Headers".
+# Adicione um novo cabeçalho: Content-Type com o valor multipart/form-data.
+# Vá para a aba "Body".
+# Selecione a opção "form-data".
+# Adicione os campos do formulário: name, email e files:
+# -Para name e email, defina o tipo como "Text" e insira os valores apropriados.
+# -Para files, defina o tipo como "File". Clique em "Select Files" e escolha os arquivos que deseja enviar. 
+#     Repita este passo para cada arquivo que deseja enviar.
+
+
 from fastapi import (
     FastAPI,
     Depends,
@@ -9,22 +22,23 @@ from fastapi import (
     Query,
     Response,
 )
+from typing import List
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from . import models
 from .database import engine, get_db
+from .models import FileRecordResponse  # Importa o modelo Pydantic para a resposta
 from evtx import PyEvtxParser
 import tempfile
 import matplotlib.pyplot as plt
 import io
+import os
 
-# Cria todas as tabelas do banco de dados
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Configuração do CORS
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
@@ -51,45 +65,54 @@ def read_upload(db: Session = Depends(get_db)):
 
 
 @app.post("/upload", status_code=status.HTTP_201_CREATED)
-async def upload_file(
+async def upload_files(
     name: str = Form(...),
     email: str = Form(...),
-    file: UploadFile = File(...),
+    files: List[UploadFile] = File(...),
     db: Session = Depends(get_db),
 ):
     try:
-        file_content = await file.read()
-        form_create = models.FileRecord(name=name, email=email, file=file_content)
-        db.add(form_create)
+        for file in files:
+            file_content = await file.read()
+            file_record = models.FileRecord(
+                name=name,
+                email=email,
+                original_filename=file.filename,
+                file=file_content,
+            )
+            db.add(file_record)
         db.commit()
-        db.refresh(form_create)
-        return {"Usuário Criado": "User create successfully"}
+        return {"detail": "Files uploaded successfully"}
     except Exception as e:
+        db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
+# @app.get("/files", response_model=List[FileRecordResponse])
+# def list_files(db: Session = Depends(get_db)):
+#     files = db.query(models.FileRecord).all() # Retorna todos os registros da tabela 'file_records'
+#     return files
 
+# "Params" no  postman, 'file_id' 
 @app.post("/download", response_class=JSONResponse)
 async def download_file(file_id: int = Query(...), db: Session = Depends(get_db)):
-    file_record = (
-        db.query(models.FileRecord).filter(models.FileRecord.id == file_id).first()
-    )
+    file_record = db.query(models.FileRecord).filter(models.FileRecord.id == file_id).first()
     if not file_record:
         raise HTTPException(status_code=404, detail="File not found")
 
-    # Cria um arquivo temporário para armazenar o conteúdo binário
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.write(file_record.file)
-    temp_file.close()
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(file_record.file)
+        temp_file_path = temp_file.name
 
-    # Extrai e converte o conteúdo do arquivo .evtx em JSON
     try:
-        caminho_arquivo = PyEvtxParser(temp_file.name)
-        records = [record for record in caminho_arquivo.records_json()]
+        parser = PyEvtxParser(temp_file_path)
+        records = [record for record in parser.records_json()]
         return JSONResponse(content=records)
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Erro ao processar o arquivo EVTX: {str(e)}"
         )
+    finally:
+        os.remove(temp_file_path)
 
 
 @app.post("/save")
@@ -104,7 +127,6 @@ async def save_file(file_id: int = Form(...), db: Session = Depends(get_db)):
     with open(file_path, "wb") as f:
         f.write(file_record.file)
 
-    # Leitura e extração de informações do arquivo
     with open(file_path, "rb") as f:
         file_content = f.read()
 
