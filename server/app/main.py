ca2db768b11a4a1421e9116b58470641cecd1927
alvarojpr <alvarojosedm9@gmail.com>
2024-07-25 12:42:51 -0300

    feat nathan

--- server/app/main.py
+++ server/app/main.py
@@ -1,16 +1,17 @@
-# NA PASTAR SERVER, E NÃO NA APP.
-# uvicorn app.main:app --reload --root-path server
-
-# Vá para a aba "Headers".
-# Adicione um novo cabeçalho: Content-Type com o valor multipart/form-data.
-# Vá para a aba "Body".
-# Selecione a opção "form-data".
-# Adicione os campos do formulário: name, email e files:
-# -Para name e email, defina o tipo como "Text" e insira os valores apropriados.
-# -Para files, defina o tipo como "File". Clique em "Select Files" e escolha os arquivos que deseja enviar. 
-#     Repita este passo para cada arquivo que deseja enviar.
-
-
+from collections import defaultdict
+import re
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from . import models
+from .database import engine, get_db
+from evtx import PyEvtxParser
+import matplotlib.pyplot as plt
+import io
+import os
+import pandas as pd
+import xmltodict
 from fastapi import (
     FastAPI,
     Depends,
@@ -22,18 +23,6 @@ from fastapi import (
     Query,
     Response,
 )
-from typing import List
-from sqlalchemy.orm import Session
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from . import models
-from .database import engine, get_db
-from .models import FileRecordResponse  # Importa o modelo Pydantic para a resposta
-from evtx import PyEvtxParser
-import tempfile
-import matplotlib.pyplot as plt
-import io
-import os
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -52,17 +41,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/forms")
 def read_upload(db: Session = Depends(get_db)):
     all_forms = db.query(models.FileRecord).all()
     return {"Todos Formulários de Contato": all_forms}
-
 
 @app.post("/upload", status_code=status.HTTP_201_CREATED)
 async def upload_files(
@@ -72,71 +58,111 @@ async def upload_files(
     db: Session = Depends(get_db),
 ):
     try:
+        user_record = models.UserRecord(name=name, email=email)
+        db.add(user_record)
+        db.commit()
+        db.refresh(user_record)  # Atualize para garantir que o ID está disponível
+
         for file in files:
             file_content = await file.read()
             file_record = models.FileRecord(
-                name=name,
-                email=email,
                 original_filename=file.filename,
                 file=file_content,
+                user_id=user_record.id
             )
             db.add(file_record)
+
         db.commit()
         return {"detail": "Files uploaded successfully"}
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
-# @app.get("/files", response_model=List[FileRecordResponse])
-# def list_files(db: Session = Depends(get_db)):
-#     files = db.query(models.FileRecord).all() # Retorna todos os registros da tabela 'file_records'
-#     return files
-
-# "Params" no  postman, 'file_id' 
+# "Params" no  postman, 'user_id'
 @app.post("/download", response_class=JSONResponse)
-async def download_file(file_id: int = Query(...), db: Session = Depends(get_db)):
-    file_record = db.query(models.FileRecord).filter(models.FileRecord.id == file_id).first()
-    if not file_record:
-        raise HTTPException(status_code=404, detail="File not found")
+async def download_file(user_id: int = Query(...), db: Session = Depends(get_db)):
+    user_record = db.query(models.UserRecord).filter(models.UserRecord.id == user_id).first()
+    if not user_record:
+        raise HTTPException(status_code=404, detail="User not found")
 
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        temp_file.write(file_record.file)
-        temp_file_path = temp_file.name
+    registros_com_erro = []
+    filtered_records_com_erro = []
+    filtered_sem_erro = []
 
-    try:
-        parser = PyEvtxParser(temp_file_path)
-        records = [record for record in parser.records_json()]
-        return JSONResponse(content=records)
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Erro ao processar o arquivo EVTX: {str(e)}"
-        )
-    finally:
-        os.remove(temp_file_path)
+    caminho_para_csv = r"C:\Users\fdisu\OneDrive\Área de Trabalho\crud-fastapi-react\server\app"
+    df_non_failure_events = pd.read_csv(os.path.join(caminho_para_csv, "1_non_failure_events.csv"), encoding='utf-8-sig', engine='python')
 
+    dict_non_failure_events = defaultdict(list)
+    for k, v in zip(df_non_failure_events.SourceName, df_non_failure_events.EventID):
+        dict_non_failure_events[k.lower()].append(v)
+    print("Non-failure events dictionary:", dict_non_failure_events)  # Verificar o dicionário de eventos não falhos
+    
+    for arquivo in user_record.files:
+        try:
+            file_content = arquivo.file
+            print("File content length:", len(file_content))  # Verificar o tamanho do arquivo
 
-@app.post("/save")
-async def save_file(file_id: int = Form(...), db: Session = Depends(get_db)):
-    file_record = (
-        db.query(models.FileRecord).filter(models.FileRecord.id == file_id).first()
-    )
-    if not file_record:
-        raise HTTPException(status_code=404, detail="File not found")
+            objeto_evtx = PyEvtxParser(io.BytesIO(file_content))
+            total_records = sum(1 for _ in objeto_evtx.records_json())
+            print("Total records:", total_records)  # Verificar o número total de registros
 
-    file_path = f"./saved_files/file_{file_id}.bin"
-    with open(file_path, "wb") as f:
-        f.write(file_record.file)
+            objeto_evtx = PyEvtxParser(io.BytesIO(file_content))  # Reiniciar o parser
+            record_count = 0
 
-    with open(file_path, "rb") as f:
-        file_content = f.read()
+            for registro in objeto_evtx.records_json():
+                record_count += 1
+                if record_count % 1000 == 0:
+                    print(f"Processing record {record_count} of {total_records}")
 
-    return {
-        "message": "File saved successfully",
-        "file_path": file_path,
-        "file_content": file_content,
-    }
+                event_level_match = re.search(r'<Level>(.*?)</Level>', registro['data'])
+                if not event_level_match:
+                    continue
+                event_level = event_level_match.group(1)
+                if ("Application" in arquivo.original_filename and event_level == "2") or \
+                        ("System" in arquivo.original_filename and event_level in ("1", "2")):
+                    registros_com_erro.append(registro)
+            print("Number of error records:", len(registros_com_erro))  # Verificar o número de registros com erro
 
+            for registro in registros_com_erro:
+                line_filtered_1 = re.sub(r'<\?xml([\s\S]*?)>\n', '', registro['data'])
+                line_filtered_2 = re.sub(r' xmlns=\"([\s\S]*?)\"', '', line_filtered_1)
+                evento_estrutura_dict = xmltodict.parse(line_filtered_2)
+                evento_estrutura_dict__tag_System = evento_estrutura_dict.get("Event").get("System")
 
+                SourceName = evento_estrutura_dict__tag_System.get("Provider").get("@Name")
+                if SourceName is None:
+                    SourceName = evento_estrutura_dict__tag_System.get("Provider").get("@EventSourceName")
+
+                EventID = evento_estrutura_dict__tag_System.get("EventID")
+
+                if SourceName is not None:
+                    print("SourceName:", SourceName)  # Verificar o SourceName
+                else:
+                    print("SourceName is None")
+
+                print("EventID:", EventID)  # Verificar o EventID
+
+                if SourceName.lower() in dict_non_failure_events:
+                    listaDe_EventIDs = dict_non_failure_events[SourceName.lower()]
+                    if int(EventID) in listaDe_EventIDs:
+                        filtered_sem_erro.append(registro)
+                        continue
+                filtered_records_com_erro.append(registro)
+
+            print("Filtered error records:", len(filtered_records_com_erro))  # Verificar o número de registros com erro filtrados
+            print("Filtered non-error records:", len(filtered_sem_erro))  # Verificar o número de registros sem erro filtrados
+
+        except Exception as e:
+            raise HTTPException(
+                status_code=500, detail=f"Erro ao processar o arquivo EVTX: {str(e)}"
+            )
+
+    return JSONResponse(content={
+        "detail": f"registros com erro: {len(filtered_records_com_erro)}\n registros sem erro: {len(filtered_sem_erro)}",
+        "registros_com_erro": filtered_records_com_erro,
+        "registros_sem_erro": filtered_sem_erro
+    })
+    
 @app.get("/plot")
 async def get_plot():
     x = [1, 2, 3, 4, 5]
@@ -153,3 +179,15 @@ async def get_plot():
     buf.seek(0)
 
     return Response(content=buf.getvalue(), media_type="image/png")
+
+# NA PASTAR SERVER, E NÃO NA APP.
+# uvicorn app.main:app --reload --root-path server
+
+# Vá para a aba "Headers".
+# Adicione um novo cabeçalho: Content-Type com o valor multipart/form-data.
+# Vá para a aba "Body".
+# Selecione a opção "form-data".
+# Adicione os campos do formulário: name, email e files:
+# -Para name e email, defina o tipo como "Text" e insira os valores apropriados.
+# -Para files, defina o tipo como "File". Clique em "Select Files" e escolha os arquivos que deseja enviar. 
+#     Repita este passo para cada arquivo que deseja enviar.
--- server/app/models.py
+++ server/app/models.py
@@ -1,10 +1,40 @@
-from sqlalchemy import Column, Integer, String, LargeBinary
+from sqlalchemy import Column, Integer, String, LargeBinary, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
+from pydantic import BaseModel
+from typing import List
 
+class UserRecord(Base):
+    __tablename__ = "user_records"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, index=True)
+    files = relationship("FileRecord", back_populates="owner")
 
 class FileRecord(Base):
     __tablename__ = "file_records"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, index=True)
+    original_filename = Column(String, index=True)
     file = Column(LargeBinary)
+    user_id = Column(Integer, ForeignKey("user_records.id"))
+    owner = relationship("UserRecord", back_populates="files")
+
+class UserRecordBase(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+class FileRecordBase(BaseModel):
+    original_filename: str
+
+    class Config:
+        orm_mode = True
+
+class UserRecordResponse(UserRecordBase):
+    id: int
+    files: List[FileRecordBase]
+
+    class Config:
+        orm_mode = True
